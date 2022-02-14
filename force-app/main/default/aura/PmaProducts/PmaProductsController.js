3d3500bf81e320751269981642a47ea9fcdc63a4
mallikharjuna-adisols <malli@adisols.com>
2022-02-15 04:02:28 +0530

    Sending AppEvent of Selected Products to Cart

--- force-app/main/default/aura/PmaProducts/PmaProductsController.js
+++ force-app/main/default/aura/PmaProducts/PmaProductsController.js
@@ -1,6 +1,6 @@
 ({
     onInit: function (component, event, helper) {
-        // Call helper to set the data for account table
+        // Call helper to set the data for Products Object
         helper.getData(component);
         var logApiResponses = true;
 
@@ -33,6 +33,21 @@
             });
             $A.enqueueAction(action);
         };
+        
+        component.fireApplicationEventCall = function (eventControllerName, message, wrapperObject) {
+            if (logApiResponses) { console.log('***  ***'); }    
+            if (logApiResponses) { console.log('***  ***' + eventControllerName); }    
+            if (logApiResponses) { console.log('*** '  + wrapperObject); }    
+            var appEvent = $A.get('e.c:' + eventControllerName);
+            appEvent.setParams({
+                "message" : message,
+                "selectedproducts":JSON.parse(JSON.stringify(wrapperObject))
+            });
+            if (logApiResponses) { console.log('*** ' + 'Sending application event' + ' ***'); }   
+            appEvent.fire();
+            if (logApiResponses) { console.log('*** ' + 'Sent application event successfully' + ' ***'); }   
+        };
+
         component.displayMessage = function (title, message, type) {
             var toastEvent = $A.get("e.force:showToast");
             toastEvent.setParams({
@@ -80,83 +95,57 @@
     },
     fireApplicationEvent : function(component, event) {
         var logApiResponses = true;
+        var message = 'Product Added Successfully';
+        var selectedProductWrapper = component.get('v.selectedProducts');
         var selectedItem = event.currentTarget;
         var selectedProdId = selectedItem.dataset.id; // Prodid
-        var selectedProdObj = JSON.stringify(selectedItem.dataset.prodObj); // Product 
-        if (logApiResponses) { console.log('after selectedProdObj ' + JSON.stringify()); }
-        if (logApiResponses) { console.table(selectedProdObj.Name); }
+        component.apiCall('getselectedProductbyId', {prodId : selectedProdId}, function(selectedproduct) {
+            
+            if(selectedProductWrapper.length >0){
+                var loop=0;
+                selectedProductWrapper.forEach(function (selectedProduct) {
+                    loop = loop+1;
+                    console.log('Loop Count::::: ' + loop + ' :::::::::');
+                    var currentprodId = selectedProduct.product.Id;
+                    if(currentprodId == selectedProdId){
+                        selectedProduct.quantity = selectedProduct.quantity + 1;
+                        if (logApiResponses) { console.log('inside already added product selectedProduct '); }
+                        if (logApiResponses) { console.table(selectedProduct); }
+                    }
+                    else
+                    {
+                        var curprod=selectedproduct;
+                        selectedProductWrapper.push(curprod);
+                         if (logApiResponses) { console.log('wrapper list not empty inside else new added product selectedProduct '); }
+                         if (logApiResponses) { console.table(selectedProduct); }
+                    }
+                    
+                    component.set('v.selectedProducts',selectedProductWrapper);
+                    if (logApiResponses) { console.log(' After Processing inside foreach selectedProductWrapper '); }
+                    if (logApiResponses) { console.table(selectedProductWrapper); }
+                    component.fireApplicationEventCall('posCommunicationEvent' , message, selectedProductWrapper );
+                });
+            }
+            else{
+                var curprod=selectedproduct;
+                selectedProductWrapper.push(curprod);
+                if (logApiResponses) { console.log('empty wrapper list add new inside else selectedProductWrapper ' ); }
+                if (logApiResponses) { console.table(selectedProductWrapper); }
+                component.set('v.selectedProducts',selectedProductWrapper);
+                component.fireApplicationEventCall('posCommunicationEvent' , message, selectedProductWrapper );
+            }
+            
+            
 
+        });
+        //component.fireApplicationEvent('posCommunicationEvent' , message, selectedProductWrapper );
         if (logApiResponses) { console.log('selectedProdId fireApplicationEvent ' + selectedProdId); }
 
-        var selectedProductWrapper = [];
-        selectedProductWrapper = component.get('v.selectedProducts');
+        
         if (logApiResponses) { console.log('after get selectedProducts selectedProductWrapper '); }
         if (logApiResponses) { console.table(selectedProductWrapper); }
         if (logApiResponses) { console.log('selectedProductWrapper.length() ' + selectedProductWrapper.length); }
-        if(selectedProductWrapper.length >0){
-            selectedProductWrapper.forEach(function (selectedProduct) {
-                var currentprodId = selectedProduct.Id;
-                if(currentprodId == selectedProdId){
-                    selectedProduct.quantity = selectedProduct.quantity + 1;
-                    if (logApiResponses) { console.log('inside already added product selectedProduct.product '); }
-                if (logApiResponses) { console.table(selectedProduct.product); }
-                }
-                else
-                {
-                    var curprod={'product' : selectedProdObj,'quantity' : 1};
-                     selectedProductWrapper.push(curprod);
-                     if (logApiResponses) { console.log('inside else new added product selectedProduct.product '); }
-                     if (logApiResponses) { console.table(selectedProduct.product); }
-                }
-                // selectedProduct.product = selectedProdObj;
-                // selectedProduct.quantity = selectedProduct.quantity + 1;
-                //selectedProduct.productPrice = selectedProduct.PricebookEntries[0].UnitPrice;
-                if (logApiResponses) { console.log('inside foreach selectedProduct.product '); }
-                if (logApiResponses) { console.table(selectedProduct.product); }
-            });
-        }
-        else{
-            var selectedProductWrapper = [];
-            var currentPrice= selectedProdObj.PricebookEntries[0].UnitPrice;
-            var currentTotalPrice= currentPrice * 1;
-            var curprod={'product' : selectedProdObj,'quantity' : 1};
-            selectedProductWrapper.push(curprod);
-            /* console.table(selectedProductWrapper); ,'productPrice' : currentPrice,'totalProductPrice' : currentTotalPrice
-            selectedProductWrapper.push({'product' : selectedProdObj});
-            selectedProductWrapper.push({'quantity' : 1});
-            selectedProductWrapper.push({'productPrice' : 1});
-            selectedProductWrapper.push({'totalProductPrice' : 1}); */
-            if (logApiResponses) { console.log('inside else selectedProductWrapper ' ); }
-            if (logApiResponses) { console.table(selectedProductWrapper); }
-        }
-        if (logApiResponses) { console.log('Outside IF selectedProductWrapper ' ); }
-            if (logApiResponses) { console.table(selectedProductWrapper); }
-        component.set('v.selectedProducts' , selectedProductWrapper);
-        selectedProductWrapper = JSON.stringify(selectedProductWrapper);
-        if (logApiResponses) { console.log('selectedProductWrapper  ' + selectedProductWrapper); }
-        /* for (var i = 0; i < productList.length; i++) {
-            var c = productList[i];
-            if (c.isSelected) {
-                selectedProducts[k] = c;
-            }
-        } */
         
-        
-       if (logApiResponses) { console.table( selectedProductWrapper); }
-      /*  if(selectedproductWrapper ==null || selectedproductWrapper==undefined || selectedproductWrapper ==''){
-        if (logApiResponses) { console.log( 'selectedProdObj ' + selectedProdObj); }
-       } */
-        component.set('v.selectedProducts',selectedProductWrapper);
-        // Get the application event by using the
-        // e.<namespace>.<event> syntax
-        var appEvent = $A.get("e.c:posCommunicationEvent");
-        appEvent.setParams({
-            "message" : selectedProdId,
-            "selectedproducts":selectedProductWrapper
-         });
-        if (logApiResponses) { console.log('*** ' + 'sending application event' + ' ***'); }    
-        
-        appEvent.fire();
     },
 
     fireApplicationEventUsingLtngSendMessage : function(cmp, event) {
--- force-app/main/default/aura/pmaCart/pmaCartController.js
+++ force-app/main/default/aura/pmaCart/pmaCartController.js
@@ -2,15 +2,91 @@
     setup: function (component, event, helper) {
         console.info('bootstrap loaded successfully.');
     },
+    onInit: function (component, event, helper) {
+        var logApiResponses = true;
+
+        component.apiCall = function (controllerMethodName, params, success, failure) {
+            var action = component.get('c.' + controllerMethodName);
+            action.setParams(params);
+            action.setCallback(this, function (data) {
+                if (logApiResponses){
+                    console.log( controllerMethodName +' Callback Response ErrorsList: ');
+                    console.table(data.getError());
+                } 
+                var errors = data.getError();
+                if (errors && Array.isArray(errors) && errors.length > 0) {
+                    if (failure) {
+                        failure(errors[0].message);
+                    } else {
+                        if (logApiResponses) {
+                            console.log( controllerMethodName +' Callback Response Error: ' );
+                            console.table( errors);
+                        }
+                        component.set('v.Loading', false);
+                    }
+                } else {
+                    if (logApiResponses){
+                        console.log( controllerMethodName +' Callback Response Success: ');
+                        console.table(data.getReturnValue());
+                    } 
+                    if (success) success(data.getReturnValue());
+                }
+            });
+            $A.enqueueAction(action);
+        };
+        
+        component.fireApplicationEvent = function (eventControllerName, message, wrapperObject) {
+            var appEvent = component.get('e.c:' + eventControllerName);
+            appEvent.setParams({
+                "message" : message,
+                "selectedproducts":JSON.stringify(wrapperObject)
+            });
+            if (logApiResponses) { console.log('*** ' + 'sending application event' + ' ***'); }    
+            
+            appEvent.fire();
+        };
+
+        component.displayMessage = function (title, message, type) {
+            var toastEvent = $A.get("e.force:showToast");
+            toastEvent.setParams({
+                "mode": 'sticky',
+                "title": title,
+                "type": type,
+                "message": message
+            });
+            toastEvent.fire();
+        };
+    },
+
     handleApplicationEvent : function (cmp, event) {
-        var selectedproducts = [];
+        var logApiResponses = true;
         var message = event.getParam("message");
-        selectedproducts = JSON.deserialize(event.getParam("selectedproducts"));
+        if (logApiResponses) { console.log('Received Message: ' + message); }
+        var selectedproductsString = event.getParam("selectedproducts");
+
+        /* cmp.apiCall('processSelectedProductsWrapper', {selectedProductsString: selectedproductsString}, function (respWrapperSelectedProducts) {
+            cmp.set("v.SelectedProductWrapper", respWrapperSelectedProducts);
+        }, function (error) {
+            //cmp.set('v.Loading', false);
+            cmp.displayMessage('Failure!', 'Failed to Set WrapperSelected Products, Please try again!!!', 'error');
+        });
+ */
+        cmp.set("v.SelectedProductWrapper", selectedproductsString);
+        var selectedproductsWrapper  = cmp.get("v.SelectedProductWrapper");
+
+        if (logApiResponses) { console.log('Received Wrapper String: '); }
+        if (logApiResponses) { console.log(selectedproductsString); }
+
+        if (logApiResponses) { console.log('Received Wrapper Object: '); }
+        if (logApiResponses) { console.table(selectedproductsWrapper); }
+
         // set the handler attributes based on event data
         cmp.set("v.messageFromEvent", message);
-        cmp.set("v.selectedproducts", selectedproducts);
+        cmp.set("v.selectedproducts", selectedproductsString);
+        
         var numEventsHandled = parseInt(cmp.get("v.numEvents")) + 1;
         cmp.set("v.numEvents", numEventsHandled);
+        cmp.displayMessage('Success!', message, 'success');
     },
 
     handleLtngSendMessageEvent : function (cmp, event) {
