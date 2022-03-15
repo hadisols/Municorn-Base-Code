4b5850fd9f2afb39acc4bb5db022f57d7774cc51
murali2805 <murali@adisols.com>
2022-03-15 11:35:25 +0530

    Webhook Signature check

--- force-app/main/default/aura/SYS_Aura_ForceSync/SYS_Aura_ForceSyncController.js
+++ force-app/main/default/aura/SYS_Aura_ForceSync/SYS_Aura_ForceSyncController.js
@@ -4,32 +4,57 @@
     },
 
     invokeFlowJS : function(component, event, helper){
+        component.set("v.messageType", "slds-text-body_small");
         component.set("v.message", 'Please wait while we sync the record:'+component.get("v.recordId"));
         
         let action = component.get("c.invokeFlow");
         action.setParams({
                             "recId" : component.get("v.recordId"),
-                            "objSetting" : component.get("v.objectSetting"),
-                            "apexHandler" : component.get("v.apexHandler")
+                            "objSetting" : event.target.getAttribute("data-objsetting"),
+                            "apexHandler" : event.target.getAttribute("data-handler"),
+                            "direction" : event.target.getAttribute("data-direction")
                         });
-        //action.setParams({"objSetting" : component.get("v.objectSetting")});
         
         action.setCallback(this,function(response){
             let state = response.getState();
             if(state === 'SUCCESS'){    
+                var sysLogStatus = response.getReturnValue().Status__c;
+                console.log('Apex response:',sysLogStatus);
+
                 var toastEvent = $A.get("e.force:showToast");
-                toastEvent.setParams({
-                    "title": "Success",
-                    "type":"success",
-                    "message": "Record successfully synced"
-                });
+
+                if(sysLogStatus.toUpperCase() == 'DONE'){
+                    toastEvent.setParams({
+                        "title": "",
+                        "type":"success",
+                        "message": "Record sync successful"
+                    });
+                }
+                else{
+                    toastEvent.setParams({
+                        "title": "",
+                        "type":"error",
+                        "message": "Record could not be synced. SYS Log Status: "+sysLogStatus
+                    });
+                }
+
                 component.set("v.message", '');
                 toastEvent.fire();
-                $A.get("e.force:closeQuickAction").fire();
+                //$A.get("e.force:closeQuickAction").fire();
                 $A.get('e.force:refreshView').fire();
                 
-            }else{
-                component.set("v.message", 'Something went wrong. Please check SYS Logs for more info');
+            }
+            else if (state === "ERROR") {
+                var errors = response.getError();
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        component.set("v.message", 'Error Details: '+errors[0].message);
+                    }
+                }
+                else{
+                    component.set("v.message", 'Error Details: Unknown error');
+                }
+                component.set("v.messageType", "slds-text-body_small slds-text-color_error");
 
             }
         });
